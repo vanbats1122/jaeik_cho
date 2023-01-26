d3684dfa241a2198003f609b34c78b392ec047c9
Allen <76972508+vanbats1122@users.noreply.github.com>
2023-01-26 18:03:17 -0500

    First Commitment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+   <>
+   <Header />
+  
+
+  <main className='main'>
+    <Home />
+  </main>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href='https://www.linkedin.com/in/allen-cho-981458a9/' className='home__social-icon' target="_blank" rel="noreferrer">
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+        <a href='https://dribbble.com/allenchu1356' className='home__social-icon' target="_blank" rel="noreferrer">
+            <i class="uil uil-dribbble"></i>
+        </a>
+        <a href='https://github.com/vanbats1122?tab=repositories' className='home__social-icon' target="_blank" rel="noreferrer">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
