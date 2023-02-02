20ef6083043812e21cc687d4739f34cd6b79be61
Allen <76972508+vanbats1122@users.noreply.github.com>
2023-02-02 12:00:43 -0500

    edit project description

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -10,7 +10,7 @@ export const Data = [
         image: Image1,
         title: "E-commerce website",
         description: 
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Combination of React JS and Tailwind CSS to visually dsiplay new stylish fall fashion. Demo of a clothing shop website fetching data from open source API. Shopping bag function at the top right corner that displays a shopping car onClick().",
         link: "https://github.com/vanbats1122/ecommerce",
     },
     {
@@ -18,7 +18,7 @@ export const Data = [
         image: Image2,
         title: "Delievery website",
         description: 
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Modern looking website for any industry. Implemented framer motion library to animate motion of images. Used Next JS and Tailwind CSS. Navigation bar is located at the top of the page. Warm tone color gives overall bright and passionate feeling to the customers.",
         link: "https://github.com/vanbats1122/delievery",
 
 
@@ -28,7 +28,7 @@ export const Data = [
         image: Image3,
         title: "Exercise dictionary",
         description: 
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Used React JS to demonstrate exercise browsing website with modern looks. Fetched Rest API from an open source. Implemented a search bar where users can search any type of exercise they are looking for. Once clicked, the website navigates to a separate page.",
         link: "https://github.com/vanbats1122/exercise",
     },
     {
@@ -36,7 +36,7 @@ export const Data = [
         image: Image4,
         title: "Admin board",
         description: 
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        "Combined React JS with Syncfusion to create an admin board that can be used in any type of businesses. Implemented customizable color and light/dark mode features to improve user's experience. It contains any types of data helpful for working environment. ",
         link: "https://github.com/vanbats1122/adminboard",
 
     },
